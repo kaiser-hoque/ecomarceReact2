0b6fdcca6efad08a81a0a8ea4141c9e35ecb514d
Ahtesanul Hoque <134125661+kaiser-hoque2@users.noreply.github.com>
2023-10-11 15:43:02 +0600

    edit

--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -294,35 +294,7 @@ function Checkout() {
               </div>
             </div>
             <div className="col-md-6">
-              <div className="row mb-5">
-                {/* <div className="col-md-12">
-                  <h2 className="h3 mb-3 text-warning">Coupon Code</h2>
-                  <div className="p-3 p-lg-5 border bg-white">
-                    <label for="c_code" className="text-black mb-3">
-                      Enter your coupon code if you have one
-                    </label>
-                    <div className="input-group w-75 couponcode-wrap">
-                      <input
-                        type="text"
-                        className="form-control me-2"
-                        id="c_code"
-                        placeholder="Coupon Code"
-                        aria-label="Coupon Code"
-                        aria-describedby="button-addon2"
-                      />
-                      <div className="input-group-append">
-                        <button
-                          className="btn btn-warning text-white"
-                          type="button"
-                          id="button-addon2 "
-                        >
-                          Apply
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div> */}
-              </div>
+              <div className="row mb-5"></div>
 
               <div className="row mb-5">
                 <div className="col-md-12">
